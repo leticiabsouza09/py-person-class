a0cf347097e58e07e085c1d9d92f3e7d58dee5a2
Letícia <leticiabsouza09@gmail.com>
2026-07-09 20:38:38 -0300

    Solution

--- app/main.py
+++ app/main.py
@@ -9,19 +9,17 @@ class Person:
         Person.people[name] = self
 
 
-def create_person_list(people_data: list[dict]) -> list[Person]:
-    # primeiro cria todas as instâncias sem relacionamentos
-    person_list = []
-    for person_dict in people_data:
-        person = Person(person_dict["name"], person_dict["age"])
-        person_list.append(person)
+def create_person_list(people_dicts: list[dict]) -> list[Person]:
+    # cria todas as instâncias com list comprehension
+    person_list = [Person(person_dict["name"], person_dict["age"])
+                   for person_dict in people_dicts]
 
-    # depois conecta marido/esposa
-    for person_dict in people_data:
+    # conecta marido/esposa usando .get()
+    for person_dict in people_dicts:
         person = Person.people[person_dict["name"]]
-        if "wife" in person_dict and person_dict["wife"] is not None:
+        if person_dict.get("wife"):
             person.wife = Person.people[person_dict["wife"]]
-        if "husband" in person_dict and person_dict["husband"] is not None:
+        if person_dict.get("husband"):
             person.husband = Person.people[person_dict["husband"]]
 
     return person_list
